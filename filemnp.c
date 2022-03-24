a9f12f652835fa04143508932d0d37aeea65255e
Hensan <pee.hensan@protonmail.com>
2022-03-24 10:55:51 -0300

    filemnp fix

--- filemnp.c
+++ filemnp.c
@@ -7,7 +7,7 @@ FILE *openImage(size_t const currentImage)
   char fileName[PATH_MAX] = "~/frames/";                // Path where the frames are
   FILE *image;
 
-  sprintf(fileName, "%04zu.jpg", currentImage);          // %04zu because I have 10⁴ frames
+  sprintf(fileName, "%s%04zu.jpg", fileName, currentImage);          // %04zu because I have 10⁴ frames
   if((image = fopen(fileName, "rb")) == NULL) {
     printf("Image could not be opened.\n");
     exit(EXIT_FAILURE);
