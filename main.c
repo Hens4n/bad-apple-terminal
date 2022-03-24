8dd77822d32bb818a23c1c8eb6670725d290249d
Hensan <pee.hensan@protonmail.com>
2022-03-24 20:21:42 -0300

    Updated

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-#include "imagemng.h"
-#include "fileman.h"
+#include "imagemnp.h"
+#include "filemnp.h"
+
 
 int main(int argc, char **argv)
 {
+  if(argc != 2) {
+    printf("The Parameter is incorrect.\n");
+    exit(EXIT_FAILURE);
+  }
 
-  return 0;
+  generateFrames(argv[1]);                                          // Generate frames from source video  
+  generateGrayFrames();                                             // Generate grayscale frames for each frame
+
+  return EXIT_SUCCESS;
 }
