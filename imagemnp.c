e02bb6dddb2f0ac100b371b8335ae0f4e40072de
Hensan <pee.hensan@protonmail.com>
2022-03-25 20:04:39 -0300

    Function to read frames is done

--- imagemnp.c
+++ imagemnp.c
@@ -4,28 +4,32 @@
 void readGenerateASCII()
 {
   u_int8_t *pixelValues = NULL;                                     // A Pointer that will hold pixel values from the image
-  size_t numberOfFrames = getFramesQuantity();                       
+  size_t numberOfFrames = getNumberOfFrames();                       
   FILE *image;
   struct jpeg_decompress_struct imageData;                          // Allocating JPEG Decompress Object
-//struct jpeg_error_mgr jErr;                                       // Error handler struct
+  struct jpeg_error_mgr jErr;                                       // Error handler struct
 
+  imageData.err = jpeg_std_error(&jErr);
   jpeg_create_decompress(&imageData);                               // Initializing JPEG Decompress Object
 
   for(size_t i = 0; i < numberOfFrames; i++) {
     image = openImage(i);
     jpeg_stdio_src(&imageData, image);                              // Reads compressed data
 
-    jpeg_read_header(&imageData, TRUE);                             // Reads JPEG header and obtain image info
+    jpeg_read_header(&imageData, TRUE);                             // Reads JPEG header and obtain image info, like Width and Height
     jpeg_start_decompress(&imageData);
 
+    char ascii[imageData.image_width+1];
     if(pixelValues == NULL) pixelValues = malloc(imageData.image_width);
 
     while(imageData.output_scanline < imageData.image_height) {
       jpeg_read_scanlines(&imageData, &pixelValues, 1);             // Reads 1 ROW of the decompressed data image and stores in the pointer
-      // Some function here to deal with convertion
-      // to ASCII and to save it in a txt file
+      getASCIILine(pixelValues, imageData.image_width, ascii);
+      printf("%s\n", ascii);
     }
-    jpeg_finish_decompress(&imageData);
+    usleep(SECONDS_PER_FRAMES);                                     // Set the FPS
+    CLEAR_SCREEN; 
+    jpeg_finish_decompress(&imageData);                             // Frees memory and makes the struct ready for another frame
     fclose(image);
   }
   jpeg_destroy_decompress(&imageData);
@@ -33,16 +37,17 @@ void readGenerateASCII()
 
 void generateFrames(char *videoFile)                                // Generates and resizes frames from video 
 {
-  char *program = "ffmpeg";
-  char *args[] = {program, "-i", videoFile, "-vf", "scale=80:-1", STORE_VFRAMES_PATH, NULL};
+  char command[MAX_PATH];
+  sprintf(command, "ffmpeg -i %s -vf scale=%d:%d %s", videoFile, TERMINAL_COLUMNS_SIZE, TERMINAL_LINES_SIZE, STORE_VFRAMES_PATH);
 
-  execvp(program, args);                                            // Check execvp from unistd.h 
+  system(command);
+  CLEAR_SCREEN;
 }
 
 void generateGrayFrames()                                           // Convert all the frames to grayscale frames
 {
-  char *program = "convert";
-  char *args[] = {program, CONVERT_FRAMES_PATH, "-colorspace", "Gray", STORE_GFRAMES_PATH, NULL};
+  char command[MAX_PATH];
+  sprintf(command, "convert %s -colorspace Gray %s", CONVERT_FRAMES_PATH, STORE_GFRAMES_PATH);
 
-  execvp(program, args);                                            // Check execvp from unistd.h
+  system(command);
 }
--- imagemnp.h
+++ imagemnp.h
@@ -5,13 +5,18 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include "filemnp.h"
+#include "ascii.h"
 #include <jpeglib.h>
 #include <unistd.h>
 
 // Change $USER for yours
 #define STORE_VFRAMES_PATH "/home/$USER/frames/%04d.jpg"            // Path to store frames extracted 
 #define STORE_GFRAMES_PATH "/home/$USER/frames/gray/%04d.jpg"       // Path to store grayscale frames
-#define CONVERT_FRAMES_PATH "/home/$USER/frames/*.jpg"              // Path to read extracted frames to conversion
+#define CONVERT_FRAMES_PATH "/home/$USER/frames/*.jpg"              // Path to read extracted frames to conversion into Grayscale
+#define CLEAR_SCREEN printf("\e[1;1H\e[2J")
+#define TERMINAL_COLUMNS_SIZE 130
+#define TERMINAL_LINES_SIZE 32
+#define SECONDS_PER_FRAMES 33333
 
 void readGenerateASCII();
 
